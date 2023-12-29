f1ac4d8203ebf434d7984ac9f5cc599f08909026
unknown <nurpeisovmansur96@gmail.com>
2023-12-29 08:59:19 +0600

    v2.1
    Эта версия от 29.12.2023 для проверки через баш

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
 import psycopg2
-try:
-    conn=4
-
-    print("db connected successfully")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM students")
-    students = cur.fetchall()
-    conn.commit()
-    print(students)
-except Exception as err:
-    print("Not connected", err)   
+dfxfxfdx
